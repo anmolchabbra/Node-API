016588b17ecf0fd8104b59a922ae4a99a1a581fe
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2023-09-14 18:44:27 -0400

    Connected to mongoDB database and added product model script and some config changes

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, ()=> {
-    console.log('Node API is ruuning on port 3000')
+
+
+mongoose.connect('mongodb+srv://admin:hFCAcMNKUXa1ij7u@curd-api.wjvox3l.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API is ruuning on port 3000')
+    })
+}).catch(() => {
+    console.log(error)
 })
 //Declare Route
 //We can even declare multiple routes as well
 app.get('/', (req, res) => {
     res.send("Hello Node API")
+})
+app.get('/world', (req, res) => {
+    res.send("Hello World")
 })
